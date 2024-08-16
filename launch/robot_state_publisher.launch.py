83c3ce2c574f9131e50613c312020c8c24eb5ab4
Eby Jose <ebyj23@gmail.com>
2024-08-16 18:21:43 +0200

    Added launching the world with the robot

--- launch/robot_state_publisher.launch.py
+++ launch/robot_state_publisher.launch.py
@@ -5,21 +5,29 @@ from launch_ros.actions import Node
 import xacro
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 
 
 def generate_launch_description():
 
     # Specify the name of the package and path to xacro file within the package
     pkg_name = 'robot_package'
-    file_subpath = '/home/gunner/mecanum_robot_ws/src/robot_package/urdf_final/urdf/urdf_final.urdf'
+    file_subpath = 'urdf/urdf'
 
 
     # Use xacro to process the file
-    xacro_file = os.path.join(file_subpath)
+    xacro_file = os.path.join(get_package_share_directory(pkg_name), file_subpath, 'urdf_final.urdf')
     robot_description_raw = xacro.process_file(xacro_file).toxml()
 
+    # Declare the world file argument
+    declare_world_cmd = DeclareLaunchArgument(
+        'world',
+        default_value=[os.path.join(get_package_share_directory(pkg_name), 'world', 'mecanum_world.world')],
+        description='Full path to the world file to load'
+    )
 
-    # Configure the node
+    # Configure the node for robot_state_publisher
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
@@ -28,20 +36,26 @@ def generate_launch_description():
                     'use_sim_time': True}] # add other parameters here if required
     )
 
+    # Include Gazebo launch file with the world file
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
             get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py']),
-        )
+        launch_arguments={'world': LaunchConfiguration('world')}.items(),
+    )
 
-
+    # Node to spawn the robot entity in Gazebo
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
                     arguments=['-topic', 'robot_description',
-                                '-entity', 'my_bot'],
+                                '-entity', 'my_bot',
+                                '-x', '0',  # Set x position
+                                '-y', '0',  # Set y position
+                                '-z', '1'],  # Set z position (e.g., 1 meter above ground)],
                     output='screen')
 
 
     # Run the node
     return LaunchDescription([
+        declare_world_cmd,
         gazebo,
         node_robot_state_publisher,
         spawn_entity
